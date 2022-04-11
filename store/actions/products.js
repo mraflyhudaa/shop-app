a204044259285c26f75501b0ba2d1c677555a8ea
mraflyhudaa <mraflyhudaa@gmail.com>
2022-04-11 21:12:20 +0700

    Add update and delete to server

--- store/actions/products.js
+++ store/actions/products.js
@@ -43,7 +43,15 @@ export const fetchProducts = () => {
 };
 
 export const deleteProduct = (productId) => {
-  return {type: DELETE_PRODUCT, pid: productId};
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-shop-guide-fb159-default-rtdb.asia-southeast1.firebasedatabase.app/products/${productId}.json`,
+      {
+        method: 'DELETE',
+      }
+    );
+    dispatch({type: DELETE_PRODUCT, pid: productId});
+  };
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
@@ -83,13 +91,29 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    },
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-shop-guide-fb159-default-rtdb.asia-southeast1.firebasedatabase.app/products/${id}.json`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title,
+          description,
+          imageUrl,
+        }),
+      }
+    );
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      },
+    });
   };
 };
